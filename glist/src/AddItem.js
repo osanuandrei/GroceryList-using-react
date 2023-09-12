f0cef47b2be292407fe71c0471352a88663a6f65
Emma Paris <osanu.andrei@yahoo.com>
2023-09-12 14:57:55 +0300

    MZ

--- glist/src/AddItem.js
+++ glist/src/AddItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useRef } from 'react'
 import { FaPlus } from 'react-icons/fa'
 const AddItem = ({newItem, handleSubmit, setnewItem}) => {
+  const inputRef = useRef();
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor='AddItem'>Add Item</label>
         <input 
        value={newItem}
+       ref={inputRef}
         onChange= {(e) => setnewItem(e.target.value)}
         autoFocus
         id='addItem'
@@ -13,6 +16,7 @@ const AddItem = ({newItem, handleSubmit, setnewItem}) => {
         placeholder='Add Item '
         required></input>
         <button
+        onClick={() => inputRef.current.focus}
         type='submit'
         aria-label='Add Item '
         >   <FaPlus />

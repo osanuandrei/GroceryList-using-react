ebd080a086d25351704f576333ccaa1033592255
Emma Paris <osanu.andrei@yahoo.com>
2023-09-14 15:17:49 +0300

    MZ

--- glist/src/App.js
+++ glist/src/App.js
@@ -3,39 +3,37 @@ import Head from './Head';
 import Footer from './Footer';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 function App() {
 
 
-  const [search,setSearch] = useState('');
-  
-  
+
+    const [search,setSearch] = useState('');
     const [items, setItems] = useState(JSON.parse(localStorage.getItem('shopping list')));
     const [newItem, setnewItem] = useState('');
-
-    const setAndSaveItems = (newItems) => {
-      setItems(newItems);
-      localStorage.setItem("shopping list", JSON.stringify(newItems));
-
-
+    useEffect ( () => {
+     localStorage.setItem('shopping list', JSON.stringify(items));
     }
+    , [items])
+
+  
     const addItem = (item) => {
       const id = items.length ? items[items.length-1].id + 1 : 1;
       const myNewItem = {id,checked:false, item: newItem}
       const listItems = [...items, myNewItem];
-      setAndSaveItems(listItems);
+      setItems(listItems);
       
 
     }
     const handleCheck = (id) => {
       const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} :item)
-      setAndSaveItems(listItems);
+      setItems(listItems);
       
     }
   
     const handleDelete = (id) => {
       const del = items.filter((item) => item.id !== id )
-      setAndSaveItems(del);
+      setItems(del);
       
      
       }
@@ -61,7 +59,7 @@ function App() {
      setSearch={setSearch}/>
 
      <Content
-     items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase())) }
+     items={items.filter((item) => ((item.item).toLowerCase()).includes(search.toLowerCase())) }
      handleDelete={handleDelete}
      handleCheck={handleCheck}
       />
